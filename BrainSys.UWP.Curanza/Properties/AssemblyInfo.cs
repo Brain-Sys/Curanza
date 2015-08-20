d054b1885c37915a44c4fdc72105ca0dc14125e9
Damiani Liborio Igor <igor.damiani@brain-sys.it>
2015-08-20 16:06:44 +0200

    Update to version 1.0.6 (Stable version)

--- BrainSys.UWP.Curanza/Properties/AssemblyInfo.cs
+++ BrainSys.UWP.Curanza/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BrainSys.UWP.Curanza")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Curanza")]
+[assembly: AssemblyDescription("Curanza Library for Universal Windows Platform")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Brain-Sys Srl")]
 [assembly: AssemblyProduct("BrainSys.UWP.Curanza")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Brain-Sys Srl 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
 [assembly: ComVisible(false)]
